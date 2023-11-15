d32e3b8d721731de0770c4af7364b686a4aa43ad
abdelwaheb-net <abdelwaheb_ammar@hotmail.fr>
2023-11-15 22:02:56 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskProgramming
+{
+    internal class Program
+    {
+
+        public static void Write(char c)
+        {
+            int i = 1000;
+            while (i-- > 1)
+            {
+                Console.Write(c);
+            }
+        }
+
+
+        public static int TestingText(object o)
+        {
+            Console.WriteLine($"Processing of task with object{o} with task id {Task.CurrentId}");
+            return o.ToString().Length;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            token.Register(() => { Console.WriteLine("Cancellation has been requested"); });
+            var t = new Task(() =>
+            {
+                int i = 0;
+                while (true)
+                {
+                    if (token.IsCancellationRequested)
+                        //token.ThrowIfCancellationRequested();
+                        //throw new OperationCanceledException();
+                        break;
+                    Console.WriteLine($"{i++}");
+
+                }
+             },token);
+            t.Start();
+
+            Task.Factory.StartNew(() => { token.WaitHandle.WaitOne(); Console.WriteLine("wait handle released,cancellation was requested"); });
+
+            Console.ReadKey();
+            cts.Cancel();
+
+            var text1 = "This";
+            var text2 = "Testing";
+
+            var Task1=new Task<int>(TestingText, text1);
+            Task<int> Task2 = Task.Factory.StartNew(TestingText, text2);
+            Task1.Start();
+
+            Console.WriteLine($"the length of {text1} is {Task1.Result}");
+            Console.WriteLine($"the length of {text2}is {Task2.Result}");
+
+           /* Task.Factory.StartNew(() => Write('.'));
+            var t = new Task(() => Write('?'));
+            t.Start();
+            Write('-');*/
+            Console.WriteLine("Main Program Done.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("TaskProgramming")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaskProgramming")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("5a1dfd0a-6695-42ae-9b75-9eaf1fff7c36")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
